f8c6952b38e741e7f6e963e70c286adc2a3fc14e
Lucas Johnson <contact@lucasjohnson.co.nz>
2022-05-05 16:53:12 +0200

    basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
+import { useForm } from 'react-hook-form';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header"></header>
-  </div>
-);
+const App = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: object): void => {
+    console.log(data);
+  };
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="First name"
+          {...register('First name', {
+            required: true,
+            pattern: /^[A-Za-z]+$/i,
+          })}
+        />
+        <input
+          type="text"
+          placeholder="Last name"
+          {...register('Last name', {
+            required: true,
+            pattern: /^[A-Za-z]+$/i,
+          })}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          {...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        <input
+          type="tel"
+          placeholder="Mobile number"
+          {...register('Mobile number', { required: true, maxLength: 13 })}
+        />
+        <input type="submit" />
+      </form>
+    </main>
+  );
+};
 
 export default App;
